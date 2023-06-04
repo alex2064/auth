ca00cdc3063ca2b5991d2a1e5d8f8e8b48accbf3
alex2064 <kimkimgodd@naver.com>
2023-06-05 01:00:05 +0900

    토큰에 User Id 추가

--- src/main/kotlin/com/example/auth/common/service/CustomUserDetailsService.kt
+++ src/main/kotlin/com/example/auth/common/service/CustomUserDetailsService.kt
@@ -1,9 +1,9 @@
 package com.example.auth.common.service
 
+import com.example.auth.common.dto.CustomUser
 import com.example.auth.member.entity.Member
 import com.example.auth.member.repository.MemberRepository
 import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
@@ -20,7 +20,8 @@ class CustomUserDetailsService(
             ?.let { createUserDetails(it) } ?: throw UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다.")
 
     private fun createUserDetails(member: Member): UserDetails =
-        User(
+        CustomUser(
+            member.id!!,
             member.loginId,
             passwordEncoder.encode(member.password),
             member.memberRole!!.map { SimpleGrantedAuthority("ROLE_${it.role}") }
--- src/main/kotlin/com/example/auth/member/controller/MemberController.kt
+++ src/main/kotlin/com/example/auth/member/controller/MemberController.kt
@@ -2,6 +2,7 @@ package com.example.auth.member.controller
 
 import com.example.auth.common.authority.TokenInfo
 import com.example.auth.common.dto.BaseResponse
+import com.example.auth.common.dto.CustomUser
 import com.example.auth.member.dto.LoginDto
 import com.example.auth.member.dto.MemberDtoRequest
 import com.example.auth.member.dto.MemberDtoResponse
@@ -37,9 +38,10 @@ class MemberController(
     /**
      * 내 정보 보기
      */
-    @GetMapping("/info/{id}")
-    fun searchMyInfo(@PathVariable id: Long): BaseResponse<MemberDtoResponse> {
-        val response = memberService.searchMyInfo(id)
+    @GetMapping("/info")
+    fun searchMyInfo(): BaseResponse<MemberDtoResponse> {
+        val userId = (SecurityContextHolder.getContext().authentication.principal as CustomUser).userId
+        val response = memberService.searchMyInfo(userId)
         return BaseResponse(data = response)
     }
 }
