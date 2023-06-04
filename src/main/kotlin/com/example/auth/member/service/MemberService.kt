83c2dc17c1d5cc9be86cda41f3434f6c1d5d6e6c
alex2064 <kimkimgodd@naver.com>
2023-06-05 00:16:01 +0900

    권한관리 / 로그인기능 추가

--- src/main/kotlin/com/example/auth/member/service/MemberService.kt
+++ src/main/kotlin/com/example/auth/member/service/MemberService.kt
@@ -1,26 +1,55 @@
 package com.example.auth.member.service
 
+import com.example.auth.common.authority.JwtTokenProvider
+import com.example.auth.common.authority.TokenInfo
 import com.example.auth.common.exception.InvalidInputException
+import com.example.auth.common.status.ROLE
+import com.example.auth.member.dto.LoginDto
 import com.example.auth.member.dto.MemberDtoRequest
 import com.example.auth.member.entity.Member
+import com.example.auth.member.entity.MemberRole
 import com.example.auth.member.repository.MemberRepository
+import com.example.auth.member.repository.MemberRoleRepository
 import jakarta.transaction.Transactional
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.stereotype.Service
 
 @Transactional
 @Service
 class MemberService(
-    private val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository,
+    private val memberRoleRepository: MemberRoleRepository,
+    private val authenticationManagerBuilder: AuthenticationManagerBuilder,
+    private val jwtTokenProvider: JwtTokenProvider,
 ) {
+    /**
+     * 회원가입
+     */
     fun signUp(memberDtoRequest: MemberDtoRequest): Boolean {
         var member: Member? = memberRepository.findByLoginId(memberDtoRequest.loginId)
         if (member != null) {
             throw InvalidInputException("loginId", "이미 등록된 ID 입니다.")
         }
 
+        // 사용자 정보 저장
         member = memberDtoRequest.toEntity()
         memberRepository.save(member)
 
+        // 권한 저장
+        val memberRole = MemberRole(null, ROLE.MEMBER, member)
+        memberRoleRepository.save(memberRole)
+
         return true
+    }
+
+    /**
+     * 로그인 -> 토근 발행
+     */
+    fun login(loginDto: LoginDto): TokenInfo {
+        val authenticationToken = UsernamePasswordAuthenticationToken(loginDto.loginId, loginDto.password)
+        val authentication = authenticationManagerBuilder.`object`.authenticate(authenticationToken)
+
+        return jwtTokenProvider.createToken(authentication)
     }
 }
